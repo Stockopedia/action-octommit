2e4ea60146d36284770f3c42aa8e2ea2445e38fe
Euan Goddard <euan@stockopedia.com>
2022-11-29 12:08:51 +0000

    fix: re-work errors are handled

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,20 @@ import {Octommit} from '@stockopedia/octommit'
 import {callAsyncFunction} from './async-function'
 
 process.on('unhandledRejection', handleError)
+main().catch(handleError)
 
 export async function main(): Promise<void> {
-  try {
-    const token = core.getInput('github-token', {required: true})
-    const script = core.getInput('script', {required: true})
+  const token = core.getInput('github-token', {required: true})
+  const script = core.getInput('script', {required: true})
 
-    const octommit = new Octommit(token)
+  const octommit = new Octommit(token)
 
-    const result = await callAsyncFunction(
-      {require, core, io, octommit},
-      script
-    )
+  const result = await callAsyncFunction({require, core, io, octommit}, script)
 
-    core.setOutput('result', result)
-  } catch (e) {
-    handleError(e)
-  }
+  core.setOutput('result', result)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function handleError(err: any): void {
   console.error(err)
   core.setFailed(`Unhandled error: ${err}`)
